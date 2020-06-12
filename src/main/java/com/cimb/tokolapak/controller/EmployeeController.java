f5ab2fc69e7997a96821c67324ed481de9c8a378
Iga Alicya <IA40093X@cimbniaga.co.id>
2020-06-12 21:37:50 +0700

    Weekend Task

--- src/main/java/com/cimb/tokolapak/controller/EmployeeController.java
+++ src/main/java/com/cimb/tokolapak/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,5 +51,9 @@ public class EmployeeController {
 		employeeService.deleteEmployeeAddress(employeeAddress.get());
 	}
 	
+	@PutMapping("/{id}")
+	public Employee updateEmployeeAddress(@RequestBody Employee employee) { 
+		 return employeeService.updateEmployeeAddress(employee);
+	}
 	
 }
--- src/main/java/com/cimb/tokolapak/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/cimb/tokolapak/service/impl/EmployeeServiceImpl.java
@@ -1,10 +1,13 @@
 package com.cimb.tokolapak.service.impl;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cimb.tokolapak.dao.EmployeeAddressRepo;
 import com.cimb.tokolapak.dao.EmployeeRepo;
+import com.cimb.tokolapak.entity.Employee;
 import com.cimb.tokolapak.entity.EmployeeAddress;
 import com.cimb.tokolapak.service.EmployeeService;
 
@@ -18,12 +21,25 @@ public class EmployeeServiceImpl implements EmployeeService {
 	private EmployeeAddressRepo employeeAddressRepo;
 	
 	@Override
+	@Transactional
 	public void deleteEmployeeAddress(EmployeeAddress employeeAddress) {
 		employeeAddress.getEmployee().setEmployeeAddress(null);
 		employeeAddress.setEmployee(null);
 		
 		employeeAddressRepo.delete(employeeAddress);
 		
+	}
+	
+	@Override
+	@Transactional
+	public Employee updateEmployeeAddress(Employee employee) {
+	
+		employee.getEmployeeAddress();
+
+		if (employee.getEmployeeAddress() == null)
+			employeeRepo.save(employee);
+
+		return employeeRepo.save(employee);
 	}
 
 }
